5e5a1ad5cad4c093a1c0b879a5ec4d201dd01a94
rathiaman <01aman.rathi@gmail.com>
2016-03-27 00:46:35 +0530

    FrontEnd Done I

---
+++ app/src/main/java/aau/corp/android/app/iitdcomplaints/ParticularPersonalComplaint.java
@@ -0,0 +1,13 @@
+package aau.corp.android.app.iitdcomplaints;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParticularPersonalComplaint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_particular_personal_complaint);
+    }
+}
