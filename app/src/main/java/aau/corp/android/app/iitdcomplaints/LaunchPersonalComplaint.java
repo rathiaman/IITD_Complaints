fb75306d94190a11dd0d9c4ac9a2d0b04db06dc7
rathiaman <01aman.rathi@gmail.com>
2016-03-27 14:05:18 +0530

    Personal Launch Screen Done With Image

--- app/src/main/java/aau/corp/android/app/iitdcomplaints/LaunchPersonalComplaint.java
+++ app/src/main/java/aau/corp/android/app/iitdcomplaints/LaunchPersonalComplaint.java
@@ -1,18 +1,27 @@
 package aau.corp.android.app.iitdcomplaints;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TableLayout;
+import android.net.Uri;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -21,15 +30,25 @@ import com.android.volley.toolbox.StringRequest;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Hashtable;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class LaunchPersonalComplaint extends AppCompatActivity {
 
     Spinner spinner_type_list, spinner_hostel_list;
     ArrayAdapter<CharSequence> type_list_adapter, hostel_list_adapter;
-    Button submit;
+    Button submit, choose_image;
     private static EditText comment_title_edittext, description_edittext,room_no_edittext,contact_edittext;
     Integer complaint_typo;
+
+    private Bitmap bitmap;
+    private int PICK_IMAGE_REQUEST = 1;
+    private String KEY_IMAGE = "image";
+    private String KEY_NAME = "name";
+    private ImageView upload_image_launch_personal_complaint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +60,6 @@ public class LaunchPersonalComplaint extends AppCompatActivity {
         type_list_adapter = ArrayAdapter.createFromResource(this, R.array.search_list, android.R.layout.simple_spinner_item);
         type_list_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_type_list.setAdapter(type_list_adapter);
-
         spinner_type_list.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -49,34 +67,22 @@ public class LaunchPersonalComplaint extends AppCompatActivity {
 
                 String selectedItem = parent.getItemAtPosition(position).toString();
 
-                if(selectedItem.equals("Electricity"))
-                {
-                    complaint_typo=1;
+                if (selectedItem.equals("Electricity")) {
+                    complaint_typo = 1;
+                } else if (selectedItem.equals("Plumber")) {
+                    complaint_typo = 2;
+                } else if (selectedItem.equals("Carpentry")) {
+                    complaint_typo = 3;
+                } else if (selectedItem.equals("Internet Issues")) {
+                    complaint_typo = 4;
+                } else if (selectedItem.equals("Sweeper")) {
+                    complaint_typo = 5;
+                } else {
+                    complaint_typo = 6;
                 }
-                else if (selectedItem.equals("Plumber"))
-                {
-                    complaint_typo =2;
-                }
-                else if (selectedItem.equals("Carpentry"))
-                {
-                    complaint_typo =3;
-                }
-                else if (selectedItem.equals("Internet Issues"))
-                {
-                    complaint_typo =4;
-                }
-                else if (selectedItem.equals("Sweeper"))
-                {
-                    complaint_typo =5;
-                }
-                else
-                {complaint_typo=6;}
 
 
             }
-
-
-
 
 
             @Override
@@ -85,20 +91,76 @@ public class LaunchPersonalComplaint extends AppCompatActivity {
             }
         });
 
-
-
-
-        submit = (Button) findViewById(R.id.submit_button);
+        choose_image = (Button) findViewById(R.id.personal_complaint_image_button);
+        choose_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFileChooser();
+            }
+        });
+        upload_image_launch_personal_complaint = (ImageView) findViewById(R.id.imageView_personal_complaint_launch);
+        submit = (Button) findViewById(R.id.submit_button_personal_complaint_launch);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                  submitNewComplaint();
+                AlertDialog.Builder submit_alert = new AlertDialog.Builder(LaunchPersonalComplaint.this);
+                submit_alert.setMessage("").setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // calls the function which send the request to the server
+                        submitNewComplaint();
+
+                    }
+                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {    // If no is pressed, you are taken back to the login screen
+                        dialog.cancel();
+                    }
+                });
+
+                AlertDialog alert = submit_alert.create();
+                alert.setTitle("Confirm Your Details  !!!");
+                alert.show();
 
             }
         });
 
     }
 
+
+
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri filePath = data.getData();
+            try {
+                //Getting the Bitmap from Gallery
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                //Setting the Bitmap to ImageView
+                upload_image_launch_personal_complaint.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getStringImage(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
 
 
     public void submitNewComplaint(){
@@ -116,49 +178,81 @@ public class LaunchPersonalComplaint extends AppCompatActivity {
         String adder1 = IPAddress.getName();
 
 
-        String url;
-        url = "http://" + adder1 + "/complaint_system/create_complaint/personal_complaint.php";
+        if(comment_title.length() == 0){
+            Toast.makeText(LaunchPersonalComplaint.this, "Title Required", Toast.LENGTH_SHORT).show();
+        }
 
-        StringRequest request = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.e("hello1", response.toString());
-                        Toast.makeText(LaunchPersonalComplaint.this, response.toString(), Toast.LENGTH_SHORT).show();
+        else if(description.length() == 0){
+            Toast.makeText(LaunchPersonalComplaint.this, "Description Required", Toast.LENGTH_SHORT).show();
+        }
 
+        else if(room_no.length() == 0){
+            Toast.makeText(LaunchPersonalComplaint.this, "Room No Required", Toast.LENGTH_SHORT).show();
+        }
 
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(LaunchPersonalComplaint.this, error.toString(), Toast.LENGTH_SHORT).show();
-                    }
-                }) {
+        else if(CheckInput.check_contact_info(contact) == false){
+            Toast.makeText(LaunchPersonalComplaint.this, "Invalid 10-digit Contact Info", Toast.LENGTH_SHORT).show();
+        }
 
-            //used to store data and sent the string request
-            @Override
-            protected LinkedHashMap<String, String> getParams() {
-                LinkedHashMap<String, String> login_credentials = new LinkedHashMap<String, String>();
-                login_credentials.put("user_id", Profile_data.getuserid());
-                login_credentials.put("first_name",Profile_data.getfirst_Name());
-                login_credentials.put("last_name",Profile_data.getlast_Name());
-                login_credentials.put("hostel",Profile_data.getHostel());
-                login_credentials.put("title",comment_title);
-                login_credentials.put("description",description);
-                login_credentials.put("contact_info",contact);
-                login_credentials.put("tags",tag_int);
-                login_credentials.put("room_no",room_no);
+        else {
+            final ProgressDialog loading = new ProgressDialog(this);
+            loading.setTitle("Submitting your Complaint");
+            loading.setMessage("Please wait");
+            loading.show();
+//            ProgressDialog.show(LaunchPersonalComplaint.this,"Uploading...","Please wait...",false,false);
+            String url;
+            url = "http://" + adder1 + "/complaint_system/create_complaint/personal_complaint.php";
 
-                return login_credentials;
-            }
+            StringRequest request = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.e("hello1", response.toString());
+                            Toast.makeText(LaunchPersonalComplaint.this, response.toString(), Toast.LENGTH_SHORT).show();
 
 
-        };;
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(LaunchPersonalComplaint.this, error.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }) {
 
-        // Add a request (in this example, called stringRequest) to your RequestQueue.
-        MySingleton.getInstance(this).addToRequestQueue(request);
+                //used to store data and sent the string request
+                @Override
+                protected LinkedHashMap<String, String> getParams() {
+                    LinkedHashMap<String, String> login_credentials = new LinkedHashMap<String, String>();
 
+                    login_credentials.put("user_id", Profile_data.getuserid());
+                    login_credentials.put("first_name", Profile_data.getfirst_Name());
+                    login_credentials.put("last_name", Profile_data.getlast_Name());
+                    login_credentials.put("hostel", Profile_data.getHostel());
+                    login_credentials.put("title", comment_title);
+                    login_credentials.put("description", description);
+                    login_credentials.put("contact_info", contact);
+                    login_credentials.put("tags", tag_int);
+                    login_credentials.put("room_no", room_no);
+
+                    if (bitmap != null){
+                        String image = getStringImage(bitmap);
+                        login_credentials.put("KEY_IMAGE", image);}
+
+                    return login_credentials;
+                }
+
+
+
+            };
+            ;
+
+            // Add a request (in this example, called stringRequest) to your RequestQueue.
+            MySingleton.getInstance(this).addToRequestQueue(request);
+            Intent in = new Intent(LaunchPersonalComplaint.this, PersonalComplaints.class);
+            startActivity(in);
+            //loading.hide();
+        }
     }
 
 }
